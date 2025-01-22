d46ed663a967ba1b69df5a05d94e5da505587717
nathan3-14 <nathan.watson.172@gmail.com>
2025-01-22 22:28:46 +0000

    some basic stuff

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element(By.NAME, "q")
+driver.get("https://www.w3schools.com/")
+# assert "Python" in driver.title
+search_bar = driver.find_element(By.ID, "search2")
+search_bar.send_keys(f"python{Keys.RETURN}")
+driver.find_element(By.ID, "accept-choices").click()
+time.sleep(2)
+# driver.find_element(By.CSS_SELECTOR, ".w3-right.w3-btn").click()
+# time.sleep(5)
 driver.close()
